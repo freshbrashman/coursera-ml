cda7d4b18e6a52ee20af168025ad7e3360cbcbed
freshbrashman <fresh.brash.sarariiman@gmail.com>
2017-11-11 01:03:58 +0900

    トレーニングデータ数による学習曲線もたぶん終わった。(なぜかm>10くらいでJcvの方が小さくなってるがこれは誤差？それとも間違い？よく分からず)

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -53,11 +53,16 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+%printf('****************************************************************\n')
+%X, y, Xval, yval, lambda
+%printf('****************************************************************\n')
 
+for i = 1:m
+    theta = trainLinearReg(X(1:i, :), y(1:i), lambda)
 
-
-
-
+    error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i), theta, lambda);
+    error_val(i) = linearRegCostFunction(Xval(1:m, :), yval(1:m), theta, lambda);
+end
 
 % -------------------------------------------------------------
 
