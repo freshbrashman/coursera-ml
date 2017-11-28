453cbc7ac61f340b154a6dc8206c50c5ebd8dcb2
freshbrashman <freshbrashman@gmail.com>
2017-11-28 17:45:10 +0900

    ex6の前半完了

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -11,7 +11,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
+% ====================== YOUR CODE HERE ====_==================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
@@ -23,11 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+valueList = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+C_candidate = 0;
+sigma_candidate = 0;
+err_candidate = -1;
 
+for C_temp = valueList
+  for sigma_temp = valueList
+    model = svmTrain(X, y, C_temp, @(x1, x2) gaussianKernel(x1, x2, sigma_temp));
+    pred = svmPredict(model, Xval);
+    err = mean(double(pred ~= yval));
 
+    if err < err_candidate | err_candidate == -1
+      C_candidate = C_temp;
+      sigma_candidate = sigma_temp;
+      err_candidate = err;
+    end
+  end
+end
 
-
-
+C = C_candidate;
+sigma = sigma_candidate;
 
 % =========================================================================
 
